b137d870caa40304d3aae1b9e273350b971bdf81
Xabc-d <jj19050@bristol.ac.uk>
2024-08-08 08:25:22 +0100

    pages

---
+++ app-worker.js
@@ -0,0 +1,117 @@
+const cacheName = "app-" + "951425fb1961f20a9c561339379b806abf6c908b";
+const resourcesToCache = ["./web/bootstrap/dist/css/bootstrap.css","/MathA","/MathA/app.css","/MathA/app.js","/MathA/manifest.webmanifest","/MathA/wasm_exec.js","/MathA/web/app.wasm","/MathA/web/bootstrap/dist/bootstrap.bundle.js","/MathA/web/logo-apple.png","/MathA/web/logo.png"];
+
+self.addEventListener("install", (event) => {
+  console.log("installing app worker 951425fb1961f20a9c561339379b806abf6c908b");
+
+  event.waitUntil(
+    caches
+      .open(cacheName)
+      .then((cache) => {
+        return cache.addAll(resourcesToCache);
+      })
+      .then(() => {
+        self.skipWaiting();
+      })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  console.log("app worker 951425fb1961f20a9c561339379b806abf6c908b is activated");
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("push", (event) => {
+  if (!event.data || !event.data.text()) {
+    return;
+  }
+
+  const notification = JSON.parse(event.data.text());
+  if (!notification) {
+    return;
+  }
+
+  const title = notification.title;
+  delete notification.title;
+
+  if (!notification.data) {
+    notification.data = {};
+  }
+  let actions = [];
+  for (let i in notification.actions) {
+    const action = notification.actions[i];
+
+    actions.push({
+      action: action.action,
+      path: action.path,
+    });
+
+    delete action.path;
+  }
+  notification.data.goapp = {
+    path: notification.path,
+    actions: actions,
+  };
+  delete notification.path;
+
+  event.waitUntil(self.registration.showNotification(title, notification));
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+
+  const notification = event.notification;
+  let path = notification.data.goapp.path;
+
+  for (let i in notification.data.goapp.actions) {
+    const action = notification.data.goapp.actions[i];
+    if (action.action === event.action) {
+      path = action.path;
+      break;
+    }
+  }
+
+  event.waitUntil(
+    clients
+      .matchAll({
+        type: "window",
+      })
+      .then((clientList) => {
+        for (var i = 0; i < clientList.length; i++) {
+          let client = clientList[i];
+          if ("focus" in client) {
+            client.focus();
+            client.postMessage({
+              goapp: {
+                type: "notification",
+                path: path,
+              },
+            });
+            return;
+          }
+        }
+
+        if (clients.openWindow) {
+          return clients.openWindow(path);
+        }
+      })
+  );
+});
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"HelloMath/src/component"
+	"github.com/maxence-charriere/go-app/v9/pkg/app"
+)
+
+func main() {
+	// Components routing:
+
+	app.Route("/", &component.Hello{Name: "123"})
+	app.Route("/hello", &component.Hello{})
+	app.Route("/week01", &component.Terms{})
+	app.Route("/week02", &component.Logic{})
+	app.Route("/week08", &component.Set{})
+	app.Route("/week09", &component.Functions{})
+	app.Route("/week10", &component.Relation{})
+	app.Route("/all", &component.All{})
+
+	app.RunWhenOnBrowser()
+	_ = app.GenerateStaticWebsite(".", &app.Handler{
+		Name:        "Hello",
+		Description: "An Hello World! example",
+		Resources:   app.GitHubPages("MathA"),
+		Icon: app.Icon{
+			Default:    "/web/logo.png",       // Specify default favicon.
+			AppleTouch: "/web/logo-apple.png", // Specify icon on IOS devices.
+		},
+		Styles: []string{
+			"./web/bootstrap/dist/css/bootstrap.css", // Loads hello.css file.
+		},
+		Scripts: []string{
+			"/web/bootstrap/dist/bootstrap.bundle.js", // Loads hello.js file.
+		},
+	})
+
+	//http.Handle("/", &app.Handler{
+	//	Name:        "Hello",
+	//	Description: "An Hello World! example",
+	//	Icon: app.Icon{
+	//		Default:    "/web/logo.png",       // Specify default favicon.
+	//		AppleTouch: "/web/logo-apple.png", // Specify icon on IOS devices.
+	//	},
+	//	Styles: []string{
+	//		"./web/bootstrap/dist/css/bootstrap.css", // Loads hello.css file.
+	//	},
+	//	Scripts: []string{
+	//		"/web/bootstrap/dist/bootstrap.bundle.js", // Loads hello.js file.
+	//	},
+	//})
+	//
+	//if err := http.ListenAndServe(":8000", nil); err != nil {
+	//	log.Fatal(err)
+	//}
+
+}
